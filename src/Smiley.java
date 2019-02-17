01e9ccdb92f1c18d9828466788d5f49008e27ac7
AipioxTechson <ritvik.bhardwaj@outlook.com>
2019-02-17 18:53:17 -0500

    Implement the Smiley class
    This commit adds in the Smiley class which reacts to the different user interactions. The Smiley class is strictly used for asthetics and will be included in the Header Panel class.

--- src/Smiley.java
+++ src/Smiley.java
@@ -1,4 +1,47 @@
-
-public class Smiley {
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+/**
+ * A class responsible for changing the Smiley Emoticon in Header Panel.
+ * @author Ritvik Bhardwaj
+ *
+ **/
+public class Smiley extends Label{
+	
+	/**
+	 * Creates an instance of Smiley.
+	 */
+	private String currentReaction;
+	public Smiley() {
+		super();
+		Image image = new Image("Images/DefaultSmiley.png");
+		ImageView iv = new ImageView(image);
+		this.setGraphic(iv);
+		this.currentReaction = "Default";
+		setMinWidth(image.getWidth());
+		setMaxWidth(image.getWidth());
+		setMinHeight(image.getHeight());
+		setMaxHeight(image.getHeight());
+	}
+	
+	/**
+	 * Update the current Smiley with reaction's image.
+	 * @param reaction: the string containing the updated image.
+	 * Precondition: reaction is a valid string name such that Images/reactionSmiley.png is a valid Image.
+	 */
+	
+	public void updateImage(String reaction) {
+		Image image = new Image("Images/" + reaction + "Smiley.png");
+		ImageView iv = new ImageView(image);
+		this.setGraphic(iv);
+		
+	}
+	/**
+	 * Return the current reaction of the Smiley.
+	 * @return currentReaction
+	 */
+	public String getReaction() {
+		return this.currentReaction;
+	}
 
 }
