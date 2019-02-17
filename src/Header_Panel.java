1ef1a7a772d7ad7d62c17efa6ced2766a728bcd2
Alexei Blinov <alexei.blinov@mail.utoronto.ca>
2019-02-16 19:18:03 -0500

    Create Header Panel to store flag count box, smiley button, and bomb color box

--- src/Header_Panel.java
+++ src/Header_Panel.java
@@ -1,6 +1,42 @@
+import javafx.geometry.Insets;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
 
-public class Header_Panel {
-	public View view;
-	public Mine_Model model;
+/**
+ * 
+ * @author Alexei
+ *
+ * A header panel to store the flag count box, the smiley button and the bomb color box.
+ *
+ * Parent class: HBox
+ * 
+ */
+public class Header_Panel extends HBox {
+	
 	public Smiley smiley;
+	
+	private StackPane flag_count_stack;
+	private Rectangle flag_count_box;
+	private Text flag_count_text;
+	
+	private Rectangle bomb_color_box;
+	
+	public Header_Panel(Smiley smiley){
+		
+		this.smiley = smiley;
+		
+		this.setPadding(new Insets(10));
+		
+		flag_count_box = new Rectangle(50, 50);
+		flag_count_text = new Text("0");
+		flag_count_stack.getChildren().addAll(flag_count_box, flag_count_text);
+		
+		bomb_color_box = new Rectangle(50, 50);
+		
+		this.getChildren().addAll(this.flag_count_stack, this.smiley, this.bomb_color_box);
+		
+	}
+	
 }
