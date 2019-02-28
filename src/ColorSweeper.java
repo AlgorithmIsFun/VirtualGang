2323026b4dee932bbad6169016d8b22cf6a5854a
AipioxTechson <ritvik.bhardwaj@outlook.com>
2019-02-28 02:14:37 -0500

    Implement Application Start
    Implements the very start of the application so the code can launch View.

--- src/ColorSweeper.java
+++ src/ColorSweeper.java
@@ -1,11 +1,27 @@
 
-public class ColorSweeper {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
+public class ColorSweeper extends Application{
+
+	View view;
+	
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Start_Screen start;
-		View view;
-		End_Screen end;
+		launch(args);
+	}
+	
+	/**
+	 * The main entry point for this application. This method is called after the init method has returned, 
+	 * and after the system is ready for the application to begin running.
+	 * @param stage - the primary stage for this application, onto which the application scene is set.
+	 * @throws Exception
+	 */
+	@Override
+	public void start(Stage stage) throws Exception {
+		// View + Controller
+		this.view = new View();
 	}
 
 }
