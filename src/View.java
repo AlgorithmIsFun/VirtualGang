157bb8a6d1930c3fc124fea56c06b9a6878f2439
Ali Ibrahim <aliibrahim@Alis-MacBook-Air.local>
2019-03-16 18:40:23 -0400

    Complete the view class
    Created the BorderPanel for the view and added the Header panel
    and grid panel to it. Also changed the appearence of border panel.
    Created a Scene to hold the view in it.

--- src/View.java
+++ src/View.java
@@ -1,24 +1,57 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class View {
+/**
+ * @author aliibrahim
+ *
+ */
+public class View extends Application {
 	private Mine_Model model;
 	private Grid_Panel grid;
 	private Header_Panel header;
+	private Smiley smiley;
+	
+	@Override
+    public void start(Stage stage) {
+	}
+	
+	/**
+	 * Constructs View class and creates stage
+	 * @param model
+	 */
 	public View(Mine_Model model) {
-		// TODO Auto-generated constructor stub
 		this.model = model;
-	}
-	void create_Grid() {}
-	
-	public View(Mine_Model model) {
-		this.model = model;
+		Stage stage = new Stage();
+		initUI(stage);
 	}
 	
-	public Grid_Panel getGridPanel() {
-		return grid;
+	/**
+	 * Creates a BorderPane and adds the Panels in their respective locations
+	 * @param stage
+	 */
+	private void initUI(Stage stage) {
+		this.smiley = new Smiley();
+		
+		this.header = new Header_Panel(this.smiley);
+		this.grid = new Grid_Panel(this.model.getBox_Grid());
+		
+		BorderPane window = new BorderPane();
+		window.setTop(this.header);
+		window.setCenter(this.grid);
+        window.setStyle("-fx-padding: 10;");
+        window.setStyle("-fx-border-style: solid inside;");
+        window.setStyle("-fx-border-width: 2;");
+        window.setStyle("-fx-border-insets: 5;");
+        window.setStyle("-fx-border-radius: 5;");
+        window.setStyle("-fx-border-color: blue;");
+        
+        
+        //creates a scene and adds the BorderPane to it
+        Scene scene = new Scene(window);
+        
+        stage.setScene(scene);
+        stage.setTitle("Color Sweeper");
 	}
-	
-	public Header_Panel getHeaderPanel() {
-		return header;
-	}
-	
 }
